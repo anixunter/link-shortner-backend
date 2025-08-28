594c676b6ae9cb8dbada8345f8c68d3313397b4c
asharma <anish.sharma@mavorion.com>
2025-08-28 11:28:13 +0545

    feat: added user self api

--- core/apps/common/views.py
+++ core/apps/common/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from core.apps.common.serializer import UserSerializer
 
@@ -14,4 +17,9 @@ class UserViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [permissions.IsAuthenticated]
         return [permission() for permission in permission_classes]
+    
+    @action(detail=False, methods=['get'])
+    def self(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
     
